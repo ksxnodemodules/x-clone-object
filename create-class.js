e209bd744acdae2761c8969dbe79bbb0bb0f9d64
KSXGitHub <hvksmr1996@gmail.com>
2016-01-28 17:01:46 +0700

    update

--- create-class.js
+++ create-class.js
@@ -2,8 +2,22 @@
 ((module) => {
 	'use strict';
 
+	var TreeCopier = require('x-iterable-tree/tree-copier');
+
 	module.exports = createClass;
 
-	function createClass() {}
+	function createClass(map) {
+
+		if (typeof map !== 'object' || !map) {
+			map = new WeakMap();
+		}
+
+		class ObjectCloner extends TreeCopier {
+
+		}
+
+		return class extends ObjectCloner {};
+
+	}
 
 })(module);
