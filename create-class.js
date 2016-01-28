f5db3055d2a5348c3d73b26e901909b1ee8974c0
KSXGitHub <hvksmr1996@gmail.com>
2016-01-28 20:19:31 +0700

    update

--- create-class.js
+++ create-class.js
@@ -6,13 +6,29 @@
 
 	module.exports = createClass;
 
-	const TRANSFORM_METHODS = (desc) => {
+	const TRANSFORM_METHODS = (map) => (desc) => {
+
+		var _process = desc.process;
 
 		return {
 			initialize: desc.initialize,
 			finalize: desc.finalize,
-			process() {}
-		}
+			process(...args) {
+				var base = _process(...args);
+				if (base) {
+					let deeper = base.deeper;
+					var _get = base.get;
+					let result = {
+						value: base.value,
+						deeper: deeper,
+						set: desc.set
+					};
+					result.get = (key) =>
+						map.get(key) || _get(value);
+					return result;
+				}
+			}
+		};
 
 	};
 
@@ -24,7 +40,7 @@
 
 		class ObjectCloner extends TreeCopier {
 
-			super(...methods.map(TRANSFORM_METHODS));
+			super(...methods.map(TRANSFORM_METHODS(map)));
 
 		}
 
