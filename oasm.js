1ea2aa62780914ab7823db40afc8db64e14f0684
KSXGitHub <hvksmr1996@gmail.com>
2016-01-31 14:13:20 +0700

    complete oasm.js

--- oasm.js
+++ oasm.js
@@ -3,5 +3,13 @@
 	'use strict';
 
 	var createClass = require('./create-class.js');
+	const CLONE_OBJECT = require('./clone-object.js');
+	const CLONE_ARRAY = require('./clone-array.js');
+	const CLONE_SET = require('./clone-set.js');
+	const CLONE_MAP = require('./clone-map.js');
+
+	class OASM extends createClass(createClass.DEFAULT_MKMAP, CLONE_OBJECT, CLONE_ARRAY, CLONE_SET, CLONE_MAP) {}
+
+	module.exports = class extends OASM {};
 
 })(module);
