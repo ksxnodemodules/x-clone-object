a7c0c83eb33fa5c350760cd29c532309904a778f
KSXGitHub <hvksmr1996@gmail.com>
2016-02-05 09:29:36 +0700

    complete clone-object

--- clone-object.js
+++ clone-object.js
@@ -4,14 +4,29 @@
 
 	var ObjectIterable = require('x-object-iterable');
 
+	var createobj = Object.create;
+	var defpro = Object.defineProperty;
+	var freezeobj = Object.freeze;
 	var getproto = Object.getPrototypeOf;
 	var objproto = Object.prototype;
 
-	const CLONE_OBJECT = Object.freeze({
+	var _return = (value) => () => value;
+
+	const RETURN = freezeobj({
+		set(container, element) {
+			defpro(container, element.descriptor);
+		}
+	});
+
+	const CLONE_OBJECT = freezeobj({
 		process(object) {
 			var proto = getproto(object);
 			if (proto === objproto || proto === null) {
-				
+				return {
+					value: new ObjectIterable(object),
+					get: _return(createobj(proto)),
+					__proto__: RETURN
+				};
 			}
 		}
 	});
